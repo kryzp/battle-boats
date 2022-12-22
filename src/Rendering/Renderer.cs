ff382aeecdb8bac06a533bdbd404a531f984087b
kryzp <kprzeczek4@gmail.com>
2022-12-22 20:15:36 +0000

    Added comments to Renderer.

--- src/Rendering/Renderer.cs
+++ src/Rendering/Renderer.cs
@@ -1,7 +1,16 @@
 namespace BattleBoats.Rendering
 {
+	/*
+	 * Holds data about the things to draw and
+	 * then draws them out in the correct order
+	 * when told to do so.
+	 */
 	public class Renderer
 	{
+		/*
+		 * Packet of data containing all the required
+		 * information to draw an image to the screen.
+		 */
 		private struct RenderPass
 		{
 			public TextImage TextImage;
@@ -10,6 +19,7 @@ namespace BattleBoats.Rendering
 			public bool OverrideEverything;
 		}
 
+		// holds all of the parts we need to draw this frame
 		private List<RenderPass> imageStack;
 
 		public Renderer()
@@ -17,6 +27,9 @@ namespace BattleBoats.Rendering
 			imageStack = new List<RenderPass>();
 		}
 
+		/*
+		 * Adds an image to the render stack.
+		 */
 		public void PushImage(TextImage img, Coordinates coords, float layer, bool overrideEverything = false)
 		{
 			imageStack.Add(new RenderPass()
@@ -28,6 +41,9 @@ namespace BattleBoats.Rendering
 			});
 		}
 
+		/*
+		 * Clears the screen
+		 */
 		public void Clear()
 		{
 			for (int y = 0; y < Program.WINDOW_HEIGHT; y++)
@@ -39,6 +55,9 @@ namespace BattleBoats.Rendering
 			}
 		}
 
+		/*
+		 * Writes out all render passes in the stack.
+		 */
 		public void Render()
 		{
 			imageStack = imageStack.OrderBy(x => x.Layer).ToList();
