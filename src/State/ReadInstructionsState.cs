fddffc1d1d318467c3d1d92b5bd82cf892f40315
kryzp <kprzeczek4@gmail.com>
2023-01-05 10:09:28 +0000

    Cleaned up instructions state

--- src/State/ReadInstructionsState.cs
+++ src/State/ReadInstructionsState.cs
@@ -14,7 +14,35 @@ namespace BattleBoats.State
 
 			menu.Add(new TextBox(new Coordinates(0, 0), (Program.WINDOW_WIDTH, Program.WINDOW_HEIGHT - 1))
 			{
-				Text = "Welcome to BattleBoats (tm), the greatest game ever made according to Albert Einstein, Stephen\nHawking and some other guy we asked on the street.\n\nBattle boats is a turn based strategy game where players eliminate their opponents fleet of\nboats by ‘firing’ at a location on a grid in an attempt to sink them. The first player to\nsink all of their opponents’ battle boats is declared the winner.\n\nEach player has two eight by eight grids. One grid is used for their own battle boats\nand the other is used to record any hits or misses placed on their opponents. At the\nbeginning of the game, players decide where they wish to place their fleet of five\nbattle boats. During game play, players take it in turns to fire at a location on their\nopponent’s board. They do this by stating the coordinates for their target. If a player\nhits their opponent's boat then this is recorded as a hit. If they miss then this is\nrecorded as a miss. The game ends when a player's fleet of boats have been sunk.\nThe winner is the player with boats remaining at the end of the game.\n\nWhen playing, the player who's current turn it is will be displayed above, in addition\nto the previous move the last player made, for reference. You can select where to\nshoot your shot on the (unknown) enemy's board when it is your turn by using the\narrow keys and pressing spacebar to confirm your shot, after which you will recieve\neither confirmation that you struck a valid hit, or be met with failure.\n\nBut first! How do you start a game? Well, if you're completely new, just press the start\nnew game button in the main menu, but if you're wanting to continue a game you already\nstarted, you can press the resume existing game button and select your correponding\nsave file. After each game, your wins/losses are archived in the /ARCHIVE/ folder where\nyou can find the games main executable, while your actual save-files can be found in /SAVES/.\n\nHappy battling!"
+				Text = "Welcome to BattleBoats (tm), the greatest game ever made according to Albert Einstein, Stephen\n" +
+				       "Hawking and some other guy we asked on the street.\n" +
+				       "\n" +
+				       "Battle boats is a turn based strategy game where players eliminate their opponents fleet of\n" +
+				       "boats by ‘firing’ at a location on a grid in an attempt to sink them. The first player to\n" +
+				       "sink all of their opponents’ battle boats is declared the winner.\n" +
+				       "\n" +
+				       "Each player has two eight by eight grids. One grid is used for their own battle boats\n" +
+				       "and the other is used to record any hits or misses placed on their opponents. At the\n" +
+				       "beginning of the game, players decide where they wish to place their fleet of five\n" +
+				       "battle boats. During game play, players take it in turns to fire at a location on their\n" +
+				       "opponent’s board. They do this by stating the coordinates for their target. If a player\n" +
+				       "hits their opponent's boat then this is recorded as a hit. If they miss then this is\n" +
+				       "recorded as a miss. The game ends when a player's fleet of boats have been sunk.\n" +
+				       "The winner is the player with boats remaining at the end of the game.\n" +
+				       "\n" +
+				       "When playing, the player who's current turn it is will be displayed above, in addition\n" +
+				       "to the previous move the last player made, for reference. You can select where to\n" +
+				       "shoot your shot on the (unknown) enemy's board when it is your turn by using the\n" +
+				       "arrow keys and pressing spacebar to confirm your shot, after which you will receive\n" +
+				       "either confirmation that you struck a valid hit, or be met with failure.\n" +
+				       "\n" +
+				       "But first! How do you start a game? Well, if you're completely new, just press the start\n" +
+				       "new game button in the main menu, but if you're wanting to continue a game you already\n" +
+				       "started, you can press the resume existing game button and select your corresponding\n" +
+				       "save file. After each game, your wins/losses are archived in the /ARCHIVE/ folder where\n" +
+				       "you can find the games main executable, while your actual save-files can be found in /SAVES/.\n" +
+				       "\n" +
+				       "Happy battling!"
 			});
 		}
 		
